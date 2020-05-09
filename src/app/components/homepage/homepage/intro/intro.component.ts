98cc561d86192f696118fcd04fdef0b9fa360a69
rterry5 <rterry5@gmail.com>
2020-05-08 18:03:11 -0700

    updated about me page

--- src/app/components/homepage/homepage/intro/intro.component.ts
+++ src/app/components/homepage/homepage/intro/intro.component.ts
@@ -11,22 +11,22 @@ export class IntroComponent implements OnInit {
     new Skill(
       `HTML`,
       // tslint:disable-next-line: max-line-length
-      `I played around with HTML since back in the MySpace days, but within the last two years I decided to jump in for fun and have really put effort into creating clean, organized markup.`
+      `I played around with HTML since back in the MySpace days, but within the last two years I decided to jump back in, and have really put effort into creating clean, organized markup.`
     ),
     new Skill(
       "CSS",
       // tslint:disable-next-line: max-line-length
-      `Since I've been more serious with developing, I learned so much in CSS that I didn't even know existed, which has helped me improve my skills in making maintainable styling code.`
+      `Since I've been more serious with developing, I've learned so much in CSS that I didn't even know existed, which has helped me improve my skills in making maintainable code.`
     ),
     new Skill(
       `JavaScipt`,
       // tslint:disable-next-line: max-line-length
-      `Only within the last year have I seriously started learning JS, something I wish I had done years ago. I'm not expert, but i've slowly been creating more projects to become comfortable creating functional websites.`
+      `Only within the last year have I seriously started learning JS, something I wish I had done years ago. I'm not an expert, but I've slowly been creating more projects to become comfortable creating functional websites.`
     ),
     new Skill(
       `Angular`,
       // tslint:disable-next-line: max-line-length
-      `I created this website in Angular after being thrown in the deep end at my job to work on Angular conversions. Initially I knew nothing, but it's becoming more manageable and fun to work in Angular. `
+      `I created this website in Angular after being thrown into the deep end at my job to work on Angular conversions. Initially I knew nothing, but it's becoming more manageable and fun to work in Angular. `
     ),
   ];
 
--- src/app/components/project-list/project-list/project/project.component.ts
+++ src/app/components/project-list/project-list/project/project.component.ts
@@ -1,37 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Project } from 'src/app/domain/project';
+import { Component, OnInit } from "@angular/core";
+import { Project } from "src/app/domain/project";
 
 @Component({
-  selector: 'app-project',
-  templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  selector: "app-project",
+  templateUrl: "./project.component.html",
+  styleUrls: ["./project.component.scss"],
 })
 export class ProjectComponent implements OnInit {
   projects: Project[] = [
     new Project(
-      'Cat Shuffle',
-      'HTML, CSS, JavaScipt/API',
-      'assets/images/projects/cats.png',
-      'Cat Shuffle was a basic one page application that I used to dabble with APIs.',
-      'https://rterry5.github.io/cats/'),
+      "Cat Shuffle",
+      "HTML, CSS, JavaScipt/API",
+      "assets/images/projects/cats.png",
+      "Cat Shuffle was a basic one page application that I used to dabble with APIs.",
+      "https://rterry5.github.io/cats/"
+    ),
     new Project(
-      'Kombucha',
-      'HTML & CSS',
-      'assets/images/projects/small-website.png',
+      "Kombucha",
+      "HTML & CSS",
+      "assets/images/projects/small-website.png",
       // tslint:disable-next-line: max-line-length
       'I had gotten an idea for a bright, fun little site from a friend who makes her own kombucha. The name "Bombucha" came from my girlfriend who suggested it as a name for the product itself.',
-      'https://rterry5.github.io/Small-Business-Website/' ),
+      "https://rterry5.github.io/Small-Business-Website/"
+    ),
     new Project(
-      'Portfolio',
-      'HTML, CSS, Angular',
-      'assets/images/projects/portfolio.png',
-      'Of course, this is the website you are currently one. I am working on creating more projects to stick here!',
-      'https://rebeccaterry.dev/')
+      "Portfolio",
+      "HTML, CSS, Angular",
+      "assets/images/projects/portfolio.png",
+      "Of course, this is the website you are currently on. I am working on creating more projects to stick here!",
+      "https://rebeccaterry.dev/"
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
