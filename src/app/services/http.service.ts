5f64c27b46a20a1c2822cc36dfbf07377afb5e11
rterry5 <rterry5@gmail.com>
2020-05-08 22:02:59 -0700

    updated header

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,28 +1,28 @@
-import { Submission } from './../domain/submission';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Subject } from 'rxjs';
+// import { Submission } from './../domain/submission';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http'
+// import { Subject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class HttpService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class HttpService {
 
-  constructor(private httpService: HttpClient) { }
+//   constructor(private httpService: HttpClient) { }
 
-  private submissions: Submission[] = [];
-  private submittedForms = new Subject<Submission[]>();
+//   private submissions: Submission[] = [];
+//   private submittedForms = new Subject<Submission[]>();
 
-  submitForm(id: number, email: string, content: string) {
-    const submission: Submission = {id: id, email: email, content: content};
-    this.submissions.push(submission);
-  }
+//   submitForm(id: number, email: string, content: string) {
+//     const submission: Submission = {id: id, email: email, content: content};
+//     this.submissions.push(submission);
+//   }
 
-  getSubmittedForms() {
-    this.httpService.get<{message: string, submissions: Submission[]}>('http://localhost:3000/api/contact')
-      .subscribe((submissionData) => {
-        this.submissions = submissionData.submissions;
-        this.submittedForms.next([...this.submissions]);
-      });
-  }
-}
+//   getSubmittedForms() {
+//     this.httpService.get<{message: string, submissions: Submission[]}>('http://localhost:3000/api/contact')
+//       .subscribe((submissionData) => {
+//         this.submissions = submissionData.submissions;
+//         this.submittedForms.next([...this.submissions]);
+//       });
+//   }
+// }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,13 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+const firebaseConfig = {
+  apiKey: "AIzaSyDctUfAOnsZK3Eu51LPL9Zbmex1DcrNesw",
+  authDomain: "portfolio-6e93a.firebaseapp.com",
+  databaseURL: "https://portfolio-6e93a.firebaseio.com",
+  projectId: "portfolio-6e93a",
+  storageBucket: "portfolio-6e93a.appspot.com",
+  messagingSenderId: "10518751794",
+  appId: "1:10518751794:web:df072600310017747a2a2b",
+  measurementId: "G-F5C2N2DCZH"
+};
