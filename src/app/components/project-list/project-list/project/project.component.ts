e72b0f9e7d57b9db33d69ebe3c67f81a1ab2caac
Rebecca Terry <rterry5@gmail.com>
2022-04-08 16:18:41 -0700

    add pokedex project

--- src/app/components/project-list/project-list/project/project.component.ts
+++ src/app/components/project-list/project-list/project/project.component.ts
@@ -10,6 +10,13 @@ import { Project } from 'src/app/domain/project';
 export class ProjectComponent implements OnInit {
   projects: Project[] = [
     new Project(
+      'Pokédex',
+      'HTML, SCSS, Angular, Javascript',
+      'assets/images/projects/pokedex.png',
+      'Currently adding new features little by little, I started creating a Pokédex to take a deeper dive into Angular and working with APIs and dynamic data',
+      'https://rterry5.github.io/pokedex/'
+    ),
+    new Project(
       'Cat Shuffle',
       'HTML, CSS, JavaScipt/API',
       'assets/images/projects/cats.png',
@@ -25,18 +32,18 @@ export class ProjectComponent implements OnInit {
       'https://rterry5.github.io/Small-Business-Website/'
     ),
     new Project(
-      'Portfolio',
-      'HTML, CSS, Angular',
-      'assets/images/projects/portfolio.png',
-      'Of course, this is the website you are currently on. I am working on creating more projects to stick here!',
-      'https://rebeccaterry.dev/'
-    ),
-    new Project(
       'Tip Calculator',
       'HTML, CSS, JavaScript',
       'assets/images/projects/tip-calculator.png',
       'For practice purposes I created a tip calculator and plan on adding more features.',
       'https://rterry5.github.io/tip-calculator/'
+    ),
+    new Project(
+      'Portfolio',
+      'HTML, CSS, Angular',
+      'assets/images/projects/portfolio.png',
+      'Of course, this is the website you are currently on. I am working on creating more projects to stick here!',
+      'https://rebeccaterry.dev/'
     ),
   ];
 
